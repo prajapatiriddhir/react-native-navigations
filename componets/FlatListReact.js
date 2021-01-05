9af82f8d66c49676ec03e4e24cc2c01b1f081075
Riddhi P <riddhi.prajapati1074@gmail.com>
2021-01-05 16:44:08 +0530

    FlatList Code Added

--- componets/FlatListReact.js
+++ componets/FlatListReact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View,TextInput, Text, SafeAreaView, FlatList, TouchableOpacity, Alert } from 'react-native'
+import { View, TextInput, Text, SafeAreaView, FlatList, TouchableOpacity, Alert } from 'react-native'
 import { Navigation } from '@react-navigation/native'
 import { color } from 'react-native-reanimated'
 import { Button } from 'native-base'
@@ -51,20 +51,6 @@ function Item({ id, name, lastname }) {
         })
         Alert.alert("Student Detalis", `${student.name}  ${student.lastname}`)
     }
-
- 
-
-const Delete = (id) =>{
-    Student.filter((stu)=>{
-        return stu.id === id;
-    })
-    Alert.alert("deleted")
-}
-
-
-
-
-
     return (
         <TouchableOpacity onPress={() => handlePress(id)}>
             <Text style={{ margin: 10, padding: 5, fontSize: 18, textAlign: 'justify', backgroundColor: 'pink' }}>{name}</Text>
@@ -76,46 +62,58 @@ const Delete = (id) =>{
 class FlatListReact extends Component {
 
     state = {
-        name:'',
-        lastname:''
+        name: '',
+        lastname: ''
     }
 
 
     submit() {
-        const {name,lastname} = this.state;
-        if (name&& lastname){
+        const { name, lastname } = this.state;
+        if (name && lastname) {
             Student.push({
-                id:Student[Student.length - 1].id,
-                name:name,
-                lastname:lastname
+                id: Student[Student.length - 1].id,
+                name: name,
+                lastname: lastname
             })
         }
-        Alert.alert("Student Added",`${this.state.name} ${this.state.lastname}`)
+        Alert.alert("Student Added", `${this.state.name} ${this.state.lastname}`)
 
     }
+
+    Delete = (id) => {
+        Student.filter((stu) => {
+            return stu.id === id;
+        })
+        Alert.alert("deleted")
+    }
+
+
+
 
     render() {
         return (
             <SafeAreaView style={{ backgroundColor: '#f08080', width: 700, height: 700, margin: 10, padding: 10 }}>
                 <Text style={{ fontSize: 20, color: '#4682b4', textAlign: 'justify' }}>FlatList Data Rendering</Text>
                 <FlatList data={Student} renderItem={({ item }) => (
-                    <Item navigation={this.props.navigation} id={item.id} name={item.name} lastname={item.lastname} 
+                    <Item navigation={this.props.navigation} id={item.id} name={item.name} lastname={item.lastname}
                     />
                 )} />
 
-             <View>
-                 <Text style={{fontSize:20,color:"black"}}>Add Student</Text>
-                 <TextInput placeholder="Enter Nmae" 
-                  onChangeText={(text) => { this.setState({ name: text })}}/>
-                 <TextInput placeholder="Enter LastName" 
-                    onChangeText={(text) => { this.setState({ lastname: text })}}/>
-                <Button primary onPress={() => { this.submit() }}><Text>Add Stu</Text></Button> 
-             </View>
+                <View>
+                    <Text style={{ fontSize: 20, color: "black" }}>Add Student</Text>
+                    <TextInput placeholder="Enter Name"
+                        onChangeText={(text) => { this.setState({ name: text }) }} />
+                    <TextInput placeholder="Enter LastName"
+                        onChangeText={(text) => { this.setState({ lastname: text }) }} />
+                    <Button style={{ width: 500 }} rounded dark onPress={()=>{ this.submit()}}><Text style={{ color:"white",padding:5,margin:5}}>Add Student</Text></Button>
+                    <TouchableOpacity onLongPress={()=>this.Delete()}>
+                    </TouchableOpacity>
+                </View>
 
             </SafeAreaView>
 
-            
-             
+
+
         )
     }
 }
